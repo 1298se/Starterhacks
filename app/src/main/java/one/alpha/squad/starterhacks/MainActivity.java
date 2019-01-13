9b33ad5313cc56ee0af111adc4a61b4c6742d0bc
1298se <oliversong7h@gmail.com>
2019-01-13 00:43:26 -0500

    linked mainactivity with homepage

--- app/src/main/java/one/alpha/squad/starterhacks/MainActivity.java
+++ app/src/main/java/one/alpha/squad/starterhacks/MainActivity.java
@@ -1,15 +1,22 @@
 package one.alpha.squad.starterhacks;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.snapchat.kit.sdk.SnapLogin;
+import com.snapchat.kit.sdk.bitmoji.ui.BitmojiIconFragment;
+import com.snapchat.kit.sdk.core.controller.LoginStateController;
 
 public class MainActivity extends AppCompatActivity {
-    private ViewGroup mViewRoot;
     private Button signInButton;
+    private ViewGroup mViewRoot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +24,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mViewRoot = findViewById(R.id.content);
-
         signInButton = (Button) findViewById(R.id.button);
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SnapLogin.getAuthTokenManager(MainActivity.this).startTokenGrant();
+                SnapLogin.getLoginStateController(getApplicationContext()).addOnLoginStateChangedListener(snapLoginListener);
+
             }
         });
-
-
-
     }
+    private LoginStateController.OnLoginStateChangedListener snapLoginListener = new LoginStateController.OnLoginStateChangedListener() {
+
+        @Override
+        public void onLoginSucceeded() {
+            Toast.makeText(MainActivity.this, "Logging Onto SnapChat...", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), homepage.class));
+        }
+
+        @Override
+        public void onLoginFailed() {
+            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onLogout() {
+            Toast.makeText(MainActivity.this, "Logging Out of SnapChat", Toast.LENGTH_SHORT).show();
+
+        }
+    };
 }
