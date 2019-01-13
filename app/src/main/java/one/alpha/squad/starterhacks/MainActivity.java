eff1fd04ec7121d32fa8c99d9151aaa583bd0483
1298se <oliversong7h@gmail.com>
2019-01-12 22:41:26 -0500

    integrated snapchat

--- app/src/main/java/one/alpha/squad/starterhacks/MainActivity.java
+++ app/src/main/java/one/alpha/squad/starterhacks/MainActivity.java
@@ -2,12 +2,19 @@ package one.alpha.squad.starterhacks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import com.snapchat.kit.sdk.SnapLogin
 
 public class MainActivity extends AppCompatActivity {
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = SnapLogin.getButton(getApplicationContext(), (ViewGroup)mViewRoot);;
+
     }
 }
