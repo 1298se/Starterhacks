ac91783bcd634f1da75ac27710c819f51c4165a6
xyolandayux <yolandayu0417@gamil.com>
2019-01-13 00:42:06 -0500

    added homepage

---
+++ app/src/main/java/one/alpha/squad/starterhacks/homepage.java
@@ -0,0 +1,17 @@
+package one.alpha.squad.starterhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.snapchat.kit.sdk.bitmoji.ui.BitmojiIconFragment;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.icon_container /* The id of the icon container */, new BitmojiIconFragment())
+                .commit();
+    }
+}
